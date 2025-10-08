b152f6762e94be5d894738775917f2ceff67be72
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-10-08 11:25:40 +0200

    refactor: API response handling to display title and copyright using both object and array formats

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
     $client = HttpClient::create();
 
     $response = $client->request('GET', "https://api.nasa.gov/planetary/apod?api_key=eSmx20HngcRpsw5jBfdVU3jQcvePfnXObw86dhUf");
-
     $content = $response->getContent();
-    $arrayData = json_decode($content, NULL, 512, JSON_OBJECT_AS_ARRAY);
 
-    var_dump($arrayData['copyright']);
+    # Avec une Class Object
+    $arrayDataClass = json_decode($content);
+    echo $arrayDataClass->title;
+
+    echo "<br><br>";
+
+    # Avec un tableau
+    $arrayDataArray = json_decode($content, NULL, 512, JSON_OBJECT_AS_ARRAY);
+    echo $arrayDataArray['copyright'];
 
 ?>
